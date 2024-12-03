2fd9edb61eea0259c1f69302f21da192cac32935
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-03 12:41:25 +0530

    Add files via upload

---
+++ bit.java
@@ -0,0 +1,15 @@
+
+public class bit{
+    public static void main(String args[]){
+       int a=7,b=8;
+        if((a&b)==0)
+        {
+            System.out.println("it is even");
+        }
+        else 
+        {
+        System.out.println("it is odd");
+        }
+    }
+}
+
