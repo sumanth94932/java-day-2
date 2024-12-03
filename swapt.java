d77c78fd82adea7710638b7798f1c62f6d457f26
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-03 13:42:22 +0530

    Add files via upload

---
+++ swapt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class swapt{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        String r=String.format("after swapping \n a=%d \n b=%d",a,b);
+        System.out.println(r);
+
+    }
+}
